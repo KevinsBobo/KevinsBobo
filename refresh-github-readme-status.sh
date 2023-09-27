220abb2239a760923526b6451a80a8f8eb920e8d
KevinsBobo <kevins.bobo@gmail.com>
2023-09-27 14:30:58 +0800

    Delete refresh-github-readme-status.sh

--- refresh-github-readme-status.sh
+++
@@ -1,3 +0,0 @@
-# Maintaining the cache through timed access
-wget --no-check-certificate -O github-status.svg "https://github-readme-stats-mu-orpin.vercel.app/api?username=kevinsbobo&show_icons=true&icon_color=1E90FF&text_color=696969&bg_color=ffffff&include_all_commits=true&hide=issues&custom_title=My%20Github%20Status%20%28include%20private%20contributions%29" > /dev/null 2>&1
-wget --no-check-certificate -O github-language.svg "https://github-readme-stats-mu-orpin.vercel.app/api/top-langs/?username=kevinsbobo&layout=compact&hide=Roff,css,html,PHP" > /dev/null 2>&1
